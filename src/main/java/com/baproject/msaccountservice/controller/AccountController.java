976b5a3a751c1f1602bf82f75dd343568af4acbc
bademba <brian.ademba@gmail.com>
2023-07-21 21:58:35 +0300

    Added List accounts endpoint

--- src/main/java/com/baproject/msaccountservice/controller/AccountController.java
+++ src/main/java/com/baproject/msaccountservice/controller/AccountController.java
@@ -5,10 +5,9 @@ import com.baproject.msaccountservice.repository.AccountRepository;
 import com.baproject.msaccountservice.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.locks.ReadWriteLock;
 
 @RestController
 @RequestMapping("/v1/account")
@@ -24,5 +23,11 @@ public class AccountController {
     @PostMapping("/")
     public ResponseEntity createAccount(@RequestBody Account account){
         return ResponseEntity.ok(accountService.createAccount(account));
+    }
+
+    //list accounts
+    @GetMapping("/")
+    public ResponseEntity<Object> listAccounts(){
+        return ResponseEntity.ok(accountService.listAccounts());
     }
 }
--- src/main/java/com/baproject/msaccountservice/service/AccountService.java
+++ src/main/java/com/baproject/msaccountservice/service/AccountService.java
@@ -5,11 +5,18 @@ import com.baproject.msaccountservice.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class AccountService {
     @Autowired
     private AccountRepository accountRepository;
+
     public Account createAccount(Account account){
         return accountRepository.save(account);
+    }
+
+    public List<Account> listAccounts(){
+        return accountRepository.findAll();
     }
 }
