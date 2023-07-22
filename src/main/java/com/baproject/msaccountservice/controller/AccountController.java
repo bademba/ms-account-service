121bb4268292f019dffc7080e27897bf3680f07e
bademba <brian.ademba@gmail.com>
2023-07-22 23:49:51 +0300

    activate/diactivate ac

--- src/main/java/com/baproject/msaccountservice/controller/AccountController.java
+++ src/main/java/com/baproject/msaccountservice/controller/AccountController.java
@@ -4,9 +4,11 @@ import com.baproject.msaccountservice.entity.Account;
 import com.baproject.msaccountservice.repository.AccountRepository;
 import com.baproject.msaccountservice.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
 import java.util.concurrent.locks.ReadWriteLock;
 
 @RestController
@@ -29,5 +31,24 @@ public class AccountController {
     @GetMapping("/")
     public ResponseEntity<Object> listAccounts(){
         return ResponseEntity.ok(accountService.listAccounts());
+    }
+
+    //activate/deactivate an account
+    @PutMapping("/activate")
+//    public Optional<> activateAccount(@RequestBody String accountNumber, String status){
+//        Optional<Account> account=accountService.getAccount(accountNumber);
+//        if(account==null){
+//            return new ResponseEntity<>(account,HttpStatus.NOT_FOUND);
+//        }
+//
+//    }
+    public ResponseEntity<Account> activateAccount(@RequestBody Account account, String accountNumber, String status){
+        Account currentAccount= accountService.findByAccountNumber(accountNumber);
+        if(currentAccount==null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        currentAccount.setStatus(account.getStatus());
+        Account updatedAccount =accountRepository.save(currentAccount);
+        return new ResponseEntity<>(updatedAccount,HttpStatus.OK);
     }
 }
--- src/main/java/com/baproject/msaccountservice/service/AccountService.java
+++ src/main/java/com/baproject/msaccountservice/service/AccountService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AccountService {
@@ -18,5 +19,12 @@ public class AccountService {
 
     public List<Account> listAccounts(){
         return accountRepository.findAll();
+    }
+
+//    public Optional<Account> getAccount(String accountNumber){
+//        return accountRepository.findByAccountNumber(accountNumber);
+//    }
+    public Account findByAccountNumber(String accountNumber){
+        return accountRepository.findByAccountNumber(accountNumber);
     }
 }
