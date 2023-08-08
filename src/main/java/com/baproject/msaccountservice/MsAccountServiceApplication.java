3aa8735b6e13274bf3e4f99525ce9f5b87f7e5cb
bademba <brian.ademba@gmail.com>
2023-08-08 19:16:20 +0300

    Added Eureka cleint discovery

--- src/main/java/com/baproject/msaccountservice/MsAccountServiceApplication.java
+++ src/main/java/com/baproject/msaccountservice/MsAccountServiceApplication.java
@@ -2,8 +2,10 @@ package com.baproject.msaccountservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsAccountServiceApplication {
 
 	public static void main(String[] args) {
